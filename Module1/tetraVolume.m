c68305b03519632ae2cc0f3f5116fae4104a7c94
Michael Searing <searing.michael@gmail.com>
2016-01-17 12:50:53 -0500

    Deleted obselete functions

--- Module1/tetraVolume.m
+++
@@ -1,6 +0,0 @@
-function volume = tetraVolume(points)
-% Find the volume of a tetrahedron wedge from the xyz locations of its
-% vertices.  Each row of points corresponds to a vertex of the tetrahedron.
-
-volume = abs(det(diff(points, 1, 1)))/6;
-end

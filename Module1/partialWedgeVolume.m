7286dc4303524782d3ac344a13aac3202d13d637
Michael Searing <searing.michael@gmail.com>
2016-01-16 23:42:07 -0500

    Volume calculation done!

--- Module1/partialWedgeVolume.m
+++ Module1/partialWedgeVolume.m
@@ -15,18 +15,15 @@ v = [points zeros(3,1); points heights];
 
 % connectivity and adjacency lists of wedge vertices
 f = [1 2 3 NaN; 4 5 6 NaN; 1 2 5 4; 2 3 6 5; 1 3 6 4];
-adjacencies = [2 3 4; 1 3 5; 1 2 6; 1 5 6; 2 4 6; 3 4 6];
+adjacencies = [2 3 4; 1 3 5; 1 2 6; 1 5 6; 2 4 6; 3 4 5];
 
-% find which of the wedge's vertices are under the plane
-underPlane = underPlanef(v(:,1), v(:,2), v(:,3));
-numUnder = sum(underPlane);
-
-patch('faces', f, 'vertices', v, 'facecolor', 'blue', 'facealpha', 0.3);
+% patch('faces', f, 'vertices', v, 'facecolor', 'blue', 'facealpha', 0.3);
+patch('faces', f, 'vertices', v, 'edgecolor', 'k', 'linewidth', 2, 'facecolor', 'none');
 view(3);
 hold on;
-[X, Y, Z] = meshgrid(linspace(min(v(:,1)), max(v(:,1)), 20), ...
-    linspace(min(v(:,2)), max(v(:,2)), 20), ...
-    linspace(min(v(:,3)), max(v(:,3)), 20));
+[X, Y, Z] = meshgrid(linspace(min(v(:,1)), max(v(:,1)), 15), ...
+    linspace(min(v(:,2)), max(v(:,2)), 15), ...
+    linspace(min(v(:,3)), max(v(:,3)), 15));
 b = underPlanef(X, Y, Z);
 s(b) = 10; % circles of size 10 for underwater points
 s(~b) = NaN; % do not plot above water points
@@ -36,33 +33,26 @@ xlabel('x');
 ylabel('y');
 zlabel('z');
 
-% list of points under the plane, offset
-% switch numUnder
-%     case {1, 2, 3}
-%         pointsUnder = v(underPlane, :);
-%     case {4, 5, 6}
-%         pointsUnder = v(~underPlane, :);
-%         volume = -wedgeVolume(points, heights); % total wedge volume
-% end
-% 
-% switch numUnder
-%     case {1, 5}
-%         P0 = repmat(pointsUnder, 3, 1);
-%         P1 = v(adjacencies(underPlane, :)', :);
-%         [intPoints, check] = planeLineIntersection(n, p, P0, P1);
-%         plotLines(P0, intPoints, 'ro-', 3);
-%         assert(all(check == 1), 'case 1 borken');
-%         volume = abs(volume + tetraVolume([pointsUnder; intPoints]));
-%     case {2, 4}
-%         
-%     case 3
-%         
-%     case 6
-%         volume = abs(volume);
-% end
-% 
-% % if we actually need the inverse of the calculated value
-% if any(numUnder == [4 5])
-%     volume = totVolume - volume;
-% end
+% find which of the wedge's vertices are under the plane
+underPlane = underPlanef(v(:,1), v(:,2), v(:,3));
+pointsUnder = v(underPlane, :);
+% numUnder = sum(underPlane);
+
+inds = find(underPlane); % indices of points under the plane
+adjs = adjacencies(underPlane, :); % indices of points adjacent to pointsUnder
+edgesAll = [repmat(inds, 3, 1) adjs(:)]; % adjacent edge pairs [underwater all]
+edgesInds = any(bsxfun(@eq, find(~underPlane)', edgesAll(:,2)), 2);
+edges = unique(sort(edgesAll(edgesInds, :), 2), 'rows');
+
+P0 = v(edges(:,1), :);
+P1 = v(edges(:,2), :);
+[intPoints, check] = planeLineIntersection(n, p, P0, P1);
+% plotLines(P0, intPoints, 'ro-', 3);
+assert(all(check == 1), 'case 1 borken');
+
+pCH = [pointsUnder; intPoints]; % convex hull points
+plot3(pCH(:,1), pCH(:,2), pCH(:,3), 'ro', 'markersize', 10, 'markerfacecolor', 'r');
+[triCH, volume] = convhull(pCH(:,1), pCH(:,2), pCH(:,3)); % convex hull indices
+trimesh(triCH, pCH(:,1), pCH(:,2), pCH(:,3), 'edgecolor', 'none', ...
+    'facecolor', 'r', 'facealpha', 0.3);
 end

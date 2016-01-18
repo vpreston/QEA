7dbb436ea77ca0b30f1539d9a0b533e62f25c38b
Victoria Preston <victorialynnpreston@gmail.com>
2016-01-18 10:53:27 -0500

    Cleaning up plot all

--- Module1/getCOM.m
+++ Module1/getCOM.m
@@ -1,7 +1,7 @@
 function [tmass,Vol,COM] = getCOM(density,wVol,wC,bVol,bC,bmass)
 %COM calculates the center of mass of a given volume and adds a wedeg to a
 %given volume
-%   density is the material density property, wVol is the volume of a wedge
+%   density is the material density property (32 kg/m^3), wVol is the volume of a wedge
 %   in m^3, and wC is the 3x1 row-wise location of the centroid of the
 %   volume. bVol is the total volume of a shape that has been clculated
 %   (for example, most of the boat hull) and bC is the 3x1 row-wise

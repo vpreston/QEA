05edf7f850f2c670ea56975bd62c4d3f84129d73
Michael Searing <searing.michael@gmail.com>
2016-02-16 20:40:26 -0500

    added normal vectors

--- Shapes3/p9donut.m
+++ Shapes3/p9donut.m
@@ -1,7 +1,7 @@
-u = linspace(0, 2*pi);
-v = linspace(0, 2*pi);
+u = linspace(0, 2*pi, 10); % short direction
+v = linspace(0, 2*pi, 30); % long direction
 [U, V] = meshgrid(u, v);
-a = 3;
+a = 4;
 r = 1;
 
 rx = (a+r*cos(U)).*cos(V);
@@ -13,3 +13,9 @@ xlabel('x');
 ylabel('y');
 zlabel('z');
 axis equal
+
+nx = cos(U).*cos(V);
+ny = cos(U).*sin(V);
+nz = sin(U);
+hold on
+quiver3(rx, ry, rz, nx, ny, nz, 0, 'LineWidth', 2, 'MaxHeadSize', 1);

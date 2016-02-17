a5559558b45551bf8ad7978ef5417bad2fbde954
Michael Searing <searing.michael@gmail.com>
2016-02-17 12:27:50 -0500

    Ready to be put into solution set!

--- Shapes3/p9donut.m
+++ Shapes3/p9donut.m
@@ -12,6 +12,7 @@ surf(rx, ry, rz);
 xlabel('x');
 ylabel('y');
 zlabel('z');
+title('Donut Normal Vectors');
 axis equal
 
 nx = cos(U).*cos(V);
@@ -19,3 +20,29 @@ ny = cos(U).*sin(V);
 nz = sin(U);
 hold on
 quiver3(rx, ry, rz, nx, ny, nz, 0, 'LineWidth', 2, 'MaxHeadSize', 1);
+
+% analytical surface area integration
+A_analytical = 2*pi*integral(@(u) r*(a+r*cos(u)), 0, 2*pi);
+disp(A_analytical);
+
+% numerical surface area integration
+[m, n] = size(rz);
+A_numerical = 0;
+for i = 1:m-1
+    for j = 1:n-1
+        % get four vertices of corners of each face
+        v0 = [rx(i,j)     ry(i,j)     rz(i,j)    ];
+        v1 = [rx(i,j+1)   ry(i,j+1)   rz(i,j+1)  ];
+        v2 = [rx(i+1,j)   ry(i+1,j)   rz(i+1,j)  ];
+        v3 = [rx(i+1,j+1) ry(i+1,j+1) rz(i+1,j+1)];
+        % get vectors describing dimensions of three corners relative to
+        % origin corner
+        a = v1 - v0;
+        b = v2 - v0;
+        c = v3 - v0;
+        % get area as half the magnitude of the cross produt
+        area = 1/2*(norm(cross(a, c)) + norm(cross(b, c)));
+        A_numerical = A_numerical + area;
+    end
+end
+disp(A_numerical);
